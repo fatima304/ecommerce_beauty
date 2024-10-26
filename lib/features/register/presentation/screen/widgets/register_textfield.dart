cdc41689a3bf690dda6299b193c60eb0d6113834
fatmaatef304 <fatimaatef990@gmailcom>
2024-10-26 11:35:02 +0300

    register screen created

---
+++ lib/features/register/presentation/screen/widgets/register_textfield.dart
@@ -0,0 +1,30 @@
+import 'package:beauty_ecommerce/core/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class RegisterTextfield extends StatelessWidget {
+  const RegisterTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomTextFormField(
+          hintText: 'Full Name',
+          preficIcon: Icons.person,
+        ),
+        CustomTextFormField(
+          hintText: 'Email',
+          preficIcon: Icons.email,
+        ),
+        CustomTextFormField(
+          hintText: 'Password',
+          preficIcon: Icons.lock,
+        ),
+        CustomTextFormField(
+          hintText: 'Confirm Password',
+          preficIcon: Icons.lock,
+        ),
+      ],
+    );
+  }
+}
